0dc4854d5d44342f61c4d13712e42a0ee9460f31
Marcel Tuchner <marcel.tuchner@worldiety.de>
2021-03-05 06:56:43 +0100

    Initial Commit

---
+++ SimpleYouTubeTest/SimpleYouTubeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleYouTubeTestApp.swift
+//  SimpleYouTubeTest
+//
+//  Created by Marcel on 05.03.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleYouTubeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
